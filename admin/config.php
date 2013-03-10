4080873b6968437777036ad8f08f90e54cd5750a
Shuddhashil Ray <rayshuddhashil@gmail.com>
2013-03-10 23:53:52 +0530

    Adding ExOnStore Base in PHP Init

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://exonstore-php.dev/admin/');
+define('HTTP_CATALOG', 'http://exonstore-php.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://exonstore-php.dev/admin/');
+define('HTTPS_CATALOG', 'http://exonstore-php.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/exonstore-php/admin/');
+define('DIR_SYSTEM', '/var/www/exonstore-php/system/');
+define('DIR_DATABASE', '/var/www/exonstore-php/system/database/');
+define('DIR_LANGUAGE', '/var/www/exonstore-php/admin/language/');
+define('DIR_TEMPLATE', '/var/www/exonstore-php/admin/view/template/');
+define('DIR_CONFIG', '/var/www/exonstore-php/system/config/');
+define('DIR_IMAGE', '/var/www/exonstore-php/image/');
+define('DIR_CACHE', '/var/www/exonstore-php/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/exonstore-php/download/');
+define('DIR_LOGS', '/var/www/exonstore-php/system/logs/');
+define('DIR_CATALOG', '/var/www/exonstore-php/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'exonstore_php_dev');
+define('DB_PREFIX', 'ex_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://exonstore-php.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://exonstore-php.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/exonstore-php/catalog/');
+define('DIR_SYSTEM', '/var/www/exonstore-php/system/');
+define('DIR_DATABASE', '/var/www/exonstore-php/system/database/');
+define('DIR_LANGUAGE', '/var/www/exonstore-php/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/exonstore-php/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/exonstore-php/system/config/');
+define('DIR_IMAGE', '/var/www/exonstore-php/image/');
+define('DIR_CACHE', '/var/www/exonstore-php/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/exonstore-php/download/');
+define('DIR_LOGS', '/var/www/exonstore-php/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'exonstore_php_dev');
+define('DB_PREFIX', 'ex_');
+?>
